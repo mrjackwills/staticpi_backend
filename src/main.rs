d4e2a1e4440c876139768c6a09bf723e0a20d841
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-10-17 22:02:17 +0100

    chore: dependencies updated

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn setup_tracing(app_env: &AppEnv) -> Result<(), ApiError> {
             .finish()
             .with(log_fmt),
     ) {
-        Ok(_) => Ok(()),
+        Ok(()) => Ok(()),
         Err(e) => {
             println!("{e:?}");
             Err(ApiError::Internal("Unable to start tracing".to_owned()))
