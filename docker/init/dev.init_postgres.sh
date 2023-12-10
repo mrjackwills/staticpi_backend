5df165f87d7df620f6ee03b9ec675d5a2007ba28
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-12-10 15:07:14 +0000

    chore: files formatted

--- docker/init/dev.init_postgres.sh
+++ docker/init/dev.init_postgres.sh
@@ -11,9 +11,9 @@ bootstrap_from_sql_file() {
 create_staticpi_user() {
 	echo "create_staticpi_user"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASSWORD';
-CREATE DATABASE $DB_NAME;
-EOSQL
+		CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASSWORD';
+		CREATE DATABASE $DB_NAME;
+	EOSQL
 }
 
 restore_pg_dump() {
@@ -21,9 +21,9 @@ restore_pg_dump() {
 	# Should use .sql instead, and then restore from backup? That way can make sure tables are setup correctly, and can also handle any migrations in an .sql file
 	pg_restore -U "$POSTGRES_USER" -O --exit-on-error --single-transaction -d "$DB_NAME" -v /init/pg_dump.tar
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_NAME;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_NAME;
-EOSQL
+		GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_NAME;
+		GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_NAME;
+	EOSQL
 }
 
 add_dev_staticpi() {
@@ -32,24 +32,23 @@ add_dev_staticpi() {
 
 	echo "granting access on staticpi to staticpi"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_NAME;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_NAME;
-EOSQL
+		GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_NAME;
+		GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_NAME;
+	EOSQL
 
 	echo "granting access on dev_staticpi to staticpi"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DEV_DB" <<-EOSQL
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_NAME;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_NAME;
-EOSQL
+		GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_NAME;
+		GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_NAME;
+	EOSQL
 }
 
-banned_domains () {
+banned_domains() {
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
-DELETE FROM banned_email_domain;
-COPY banned_email_domain (domain) FROM '/init/banned_domains.txt';
-EOSQL
+		DELETE FROM banned_email_domain;
+		COPY banned_email_domain (domain) FROM '/init/banned_domains.txt';
+	EOSQL
 }
-
 
 from_scratch() {
 	create_staticpi_user
@@ -63,18 +62,14 @@ from_pg_dump() {
 	banned_domains
 }
 
-main () {
+main() {
 
-	if [ -f "/init/pg_dump.tar" ];
-	then
-		from_pg_dump;
+	if [ -f "/init/pg_dump.tar" ]; then
+		from_pg_dump
 	else
-		from_scratch;
+		from_scratch
 	fi
 	add_dev_staticpi
 }
 
 main
-
-
-
