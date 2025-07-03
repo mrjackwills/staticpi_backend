c6742873c5a4afc06051151d6158461712475527
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-07-03 08:22:46 +0100

    chore: create_release.sh updated

--- create_release.sh
+++ create_release.sh
@@ -371,7 +371,7 @@ release_flow() {
 }
 
 build_choice() {
-	cmd=(dialog --backtitle "Choose option" --radiolist "choose" 14 80 16)
+	cmd=(dialog --backtitle "Choose option" --keep-tite --radiolist "choose" 14 80 16)
 	options=(
 		1 "x86 linux gnu" off
 		2 "aarch64 linux gnu" off
@@ -405,7 +405,7 @@ build_choice() {
 }
 
 build_container_choice() {
-	cmd=(dialog --backtitle "Choose option" --radiolist "choose" 14 80 16)
+	cmd=(dialog --backtitle "Choose option" --keep-tite --radiolist "choose" 14 80 16)
 	options=(
 		1 "x86 " off
 		2 "aarch64" off
@@ -439,7 +439,7 @@ build_container_choice() {
 }
 
 main() {
-	cmd=(dialog --backtitle "Choose option" --radiolist "choose" 14 80 16)
+	cmd=(dialog --backtitle "Choose option" --keep-tite --radiolist "choose" 14 80 16)
 	options=(
 		1 "test" off
 		2 "release" off
