08420b2c911392843a1d31bc720bf55f7fc95ebc
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-07-03 08:21:27 +0100

    chore: dependencies updated

--- src/servers/ws/ws_router.rs
+++ src/servers/ws/ws_router.rs
@@ -246,6 +246,7 @@ impl WsRouter {
     }
 
     /// Deal with text messages when structured data is enabled
+	#[allow(clippy::cognitive_complexity)]
     async fn structured_text_handler(input: &HandlerData<'_>, data: Utf8Bytes) {
         match input.device_type {
             ConnectionType::Client => match serde_json::from_str::<wm::ClientBody>(&data) {
@@ -383,7 +384,8 @@ impl WsRouter {
             .on_upgrade(move |socket| Self::online_message_handler(socket, state))
     }
 
-    // Send an api_version & uptime response, then close socket
+    /// Send an api_version & uptime response, then close socket
+	#[allow(clippy::cognitive_complexity)]
     async fn online_message_handler(mut socket: WebSocket, state: ApplicationState) {
         if let Ok(response) = serde_json::to_string(&oj::Online {
             uptime: calc_uptime(state.start_time),
